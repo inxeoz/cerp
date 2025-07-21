ebab4b9fb5222fa0f29db00ce81de37dd44ef686
inxeoz@inxeoz.com <inxeoz@inxeoz.com>
2025-07-21 23:45:39 +0530

    create button

--- cerp/procurement_module/doctype/requirement_document/requirement_document.js
+++ cerp/procurement_module/doctype/requirement_document/requirement_document.js
@@ -6,3 +6,25 @@
 
 // 	},
 // });
+
+
+// frappe.ui.form.on('Requirement Document', {
+// 	refresh(frm) {
+// 		// your code here
+// 	}
+// })
+
+frappe.ui.form.on("Requirement Document", {
+    refresh(frm) {
+        if (frm.doc.workflow_state === "Approved by MD Sir") {
+            frm.add_custom_button("Create Bid Form", function() {
+                // Your logic to create bid form goes here
+                
+                  frappe.new_doc("Bid Creation Form", {
+                    // Replace 'requirement' with actual fieldname in Bid Form
+                    requirements: frm.doc.name
+                });
+            });
+        }
+    }
+});
