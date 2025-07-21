506772567cef652ba7c38feb92ad282f6d083e8f
inxeoz@inxeoz.com <inxeoz@inxeoz.com>
2025-07-21 23:28:49 +0530

    modules

---
+++ cerp/procurement_module/doctype/committee_member/test_committee_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, inxeoz and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCommitteeMember(FrappeTestCase):
+	pass
---
+++ cerp/procurement_module/doctype/minutes_of_meeting/minutes_of_meeting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, inxeoz and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Minutes of Meeting", {
+// 	refresh(frm) {
+
+// 	},
+// });
