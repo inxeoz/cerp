751c5abc0eba0c2757c9275a69e0ffd926d097f1
inxeoz@inxeoz.com <inxeoz@inxeoz.com>
2025-07-24 17:42:14 +0530

    user info custom html block

--- cerp/procurement_module/setup/procurement_module_setup/bid_doc_html_block_create.py
+++ cerp/procurement_module/setup/procurement_module_setup/bid_doc_html_block_create.py
@@ -3,10 +3,42 @@ from cerp.procurement_module.setup.default_setup_methods.create_html_block impor
 
 def name_email_create_html_block():
     return create_html_block(
-        name="my_alert",
-        html="<div class='alert'>Hello World!</div>",
-        css=".alert { background: yellow; padding: 10px; }",
-        js="console.log('Alert loaded');"
+        name="info_about_user",
+
+        html="""<div id="user-info">
+                    <div id="full_name"></div>
+                    <div id="email"></div>
+                    </div>""",
+
+        css="""#user-info {
+                display: flex;
+                gap: 10px; /* space between name and email */
+                align-items: center; /* vertically center items */
+                }
+
+                #full_name, #email {
+                padding: 8px 15px;
+                background-color: #cce5ff; /* subtle blue background */
+                border-radius: 6px;
+                font-family: Arial, sans-serif;
+                font-size: 18px; /* increased font size */
+                color: #1a1a1a;
+                font-weight: 600;
+                }""",
+
+        js="""frappe.call({
+                method: "frappe.client.get",
+                    args: {
+                        doctype: "User",
+                        name: frappe.session.user,
+                        },
+                        callback(r) {
+                            if(r.message) {
+                                root_element.querySelector("#full_name").textContent=r.message.full_name;
+                                root_element.querySelector('#email').textContent=r.message.email;
+                            }
+                        }
+            });"""
     )
 
 
